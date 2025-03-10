2a48c6790c0f73ee0653f4c3c1be8025b40f6da1
ezioborges <eziopborges@gmail.com>
2025-03-10 11:43:03 -0300

    CREATE USERS: methods GET and POST to route /users

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,7 +1,27 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/', async () => {
-    console.log('aqui ta vindo!')
+    const users = await knex('users').select()
+
+    return { users }
+  })
+
+  app.post('/', async (req, res) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+    })
+
+    const { name } = createUserBodySchema.parse(req.body)
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+    })
+
+    return res.status(201).send()
   })
 }
