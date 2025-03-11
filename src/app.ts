2cac6b4a1376cf6c47320f199ffe84651cda4665
ezioborges <eziopborges@gmail.com>
2025-03-11 09:25:35 -0300

    insert sessionId in users AND config fastify/cookies

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
 import { usersRoutes } from './routes/usersRoutes'
+import cookie from '@fastify/cookie'
 
 export const app = fastify()
 
+app.register(cookie)
+
 app.register(usersRoutes, {
-    prefix: 'users'
+  prefix: 'users',
 })
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -2,10 +2,13 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
 import { randomUUID } from 'node:crypto'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function usersRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const users = await knex('users').select()
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (req) => {
+    const { sessionId } = req.cookies
+
+    const users = await knex('users').where('session_id', sessionId).select()
 
     return { users }
   })
@@ -29,9 +32,21 @@ export async function usersRoutes(app: FastifyInstance) {
 
     const { name } = createUserBodySchema.parse(req.body)
 
+    let sessionId = req.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      res.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('users').insert({
       id: randomUUID(),
       name,
+      session_id: sessionId,
     })
 
     return res.status(201).send()
