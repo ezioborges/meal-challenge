6fd15a8435717ee09e93955e98a024aedff7214c
ezioborges <eziopborges@gmail.com>
2025-03-12 10:47:18 -0300

    GET metrics: /GET method

--- src/routes/metricRutes.ts
+++ src/routes/metricRutes.ts
@@ -13,10 +13,17 @@ export async function metricRoutes(app: FastifyInstance) {
     const [user] = await knex('users').where('id', id).select()
     const userMealList = await knex('meals').where('user_id', id).select()
 
+    // 0 - false
+    // 1 - true
+    const helthyMeal = userMealList.filter((meal) => meal.inTheDiet === 1)
+    const badMeal = userMealList.filter((meal) => meal.inTheDiet === 0)
+
     const response = {
       userId: user.id,
       userName: user.name,
       userMealAmount: userMealList.length,
+      userMealHelthy: helthyMeal.length,
+      userMealNotHelthy: badMeal.length,
     }
 
     return response
